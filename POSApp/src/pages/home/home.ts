905e7f20d6c6315ceff8d6529b29ee0fb1077860
Steven Soranno <ssoranno>
2019-03-01 14:59:48 -0500

    figured out the problem with firebaseDB

--- POSApp/src/pages/home/home.ts
+++ POSApp/src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { resolveDefinition } from '@angular/core/src/view/util';
+
+//import { FirebaseListObservable } from '@angular/fire/database/observable';
 
 /**
  * Generated class for the HomePage page.
@@ -15,22 +18,22 @@ import { AngularFireDatabase } from '@angular/fire/database';
   templateUrl: 'home.html',
 })
 export class HomePage {
-
+  payments: Array<any> = [];
   constructor(public navCtrl: NavController, public navParams: NavParams, public fdatabase: AngularFireDatabase) {
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
-    this.fdatabase.database.ref('/payments/7k0x90oIGPVoyHF78Enu7u4DAGT2').once('value').then(function(snapshot) {
-      console.log(snapshot.val());
-      var maxAmount = 0;
-      for(var id in snapshot.val()){
-        console.log(id);
-        if(snapshot.child(id).val().amount > maxAmount){
-          maxAmount = snapshot.child(id).val().amount;
-        }
-      }
-      console.log(maxAmount);
+
+    this.fdatabase.database.ref('/payments/7k0x90oIGPVoyHF78Enu7u4DAGT2').once('value')
+      .then(snapshot => {
+      
+        snapshot.forEach( itemSnap => {
+          console.log(itemSnap.val());
+          this.payments.push(itemSnap.val());
+        });
+        console.log(this.payments);
     });
   }
 
